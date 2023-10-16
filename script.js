46706b0d86d561d91c822fb35d6b5aa266a7262d
Caic Rangel <caicrangel@gmail.com>
2023-10-16 14:09:18 -0300

    Adaptando slider para passar automaticamente

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let count =1;
-document.getElementsById("radio1").checked = true;
 
-setInterval(function(){
-},2000)
+let count = 1;
 
-
-function nextImage(){
+function nextImage() {
     count++;
-    if(count>4){
+    if (count > 4) {
         count = 1;
     }
 
-    document.getElementsById("radio"+count).checked = true;
+    document.getElementById("radio" + count).checked = true;
 }
+
+document.getElementById("radio1").checked = true;
+
+setInterval(nextImage, 3000); // Mudei o intervalo para 3000ms (3 segundos)
+
+/*Codigo Raquel avançando com clic*/
+
+// let count =1;
+// document.getElementsById("radio1").checked = true;
+
+// setInterval(function(){
+// },2000)
+
+
+// function nextImage(){
+//     count++;
+//     if(count>4){
+//         count = 1;
+//     }
+
+//     document.getElementsById("radio"+count).checked = true;
+// }
