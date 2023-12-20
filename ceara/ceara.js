389b407322270f76f08872b4cb3115fa1fe0a840
Caic Rangel <caicrangel@gmail.com>
2023-12-20 08:57:15 -0300

    Ajustando a estrutura de Ceara

---
+++ ceara/ceara.js
@@ -0,0 +1,105 @@
+const arrayDicasRestaurantes = [
+  {
+    imagem:"/ceara/imgCeara/img_ceara_redimensionadas/pontoDoBaiao.jpg",
+    nomeRestaurante: "Ponto do Baião",
+    descricaoRestaurante:"O restaurante Ponto do Baião é tradicional entre as opções de estabelecimentos em Fortaleza com foco na culinária regional e churrascos. Há mais de uma década no mercado, a casa conta com a tradicional quinta do caranguejo, além de shows de humor para agradar ao público.<br><br><br><br><br><br><br><br><br><br><br><br><br> Rua VII, 229 - Quintino Cunha,<br> Fortaleza",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3739549704305!2d-38.603673025026275!3d-3.728388896245541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74a527ccb0a4d%3A0x6397c011e5a09e06!2sPonto%20do%20Bai%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1702994701515!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/ceara/imgCeara/img_ceara_redimensionadas/raizCozinhaBrasileira.jpg",
+    nomeRestaurante: "Raiz Cozinha Brasileira",
+    descricaoRestaurante:"Se gosta da culinária Brasileira, vale à pena visitar este restaurante. Restaurante Raiz Cozinha Brasileira tem pizza saborosa, tamboril bom e Carne de sol fascinante. Os funcionários pacientes recebem clientes durante o ano inteiro. É sempre agradável comer aqui por causa do serviço bom. Espere um ambiente caseiro neste restaurante.<br><br><br><br><br><br><br><br><br><br><br><br> Av. Washington Soares, 3300 - Edson Queiroz, Fortaleza",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.104340940372!2d-38.483634325025875!3d-3.7874630961863884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c745c69749b90d%3A0x9f16c9c3d125dcef!2sRaiz%20Cozinha%20Brasileira%3A%20Restaurante%2C%20Carnes%2C%20Camar%C3%A3o%2C%20Happy%20Hour%20em%20Fortaleza!5e0!3m2!1spt-BR!2sbr!4v1702995991219!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/ceara/imgCeara/img_ceara_redimensionadas/cortileRistorante.jpg",
+    nomeRestaurante: "Cortile Ristorante",
+    descricaoRestaurante:"Restaurante italiano moderno unindo os maiores prazer da vida: a conversa, o encontro e a comida. Se depare com uma casa moderna, aconchegante e descontraida na nossa varanda ou salão interno. A sinergia entre a sala, a cozinha e o bar. Coma bem, ria com frequência, ame muito! Sinta-se em casa!<br><br><br><br><br><br><br><br><br><br><br><br><br> Rua Vicente Leite, 520 - Meireles, <br> Fortaleza",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15662743.755302008!2d-55.490738170879624!3d-16.580934145003265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7496441317811%3A0x8d7c97448cba3e42!2sCortile%20Ristorante!5e0!3m2!1spt-BR!2sbr!4v1702996780218!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem:"/ceara/imgCeara/img_ceara_redimensionadas/ryoriBuganvilia.jpg",
+    nomeRestaurante: "Ryori Shopping Buganvília",
+    descricaoRestaurante:"O Restaurante Ryori é conhecido por oferecer o melhor da gastronomia oriental, com uma cozinha moderna, sofisticada e criativa. No ano de 2018 completamos 15 anos, em comemoração à essa data especial, inovamos a nossa marca com uma nova roupagem, porém, cheia de tradição.<br> A palavra Ryori é originada da expressão japonesa “Ryouri“, que faz referência ao ato de cozinhar. Na cultura japonesa, é síntese de harmonia, equilíbrio e valorização do momento.<br> O Ryori oferece um cardápio surpreendente, ousado e com toque de sofisticação que vai muito além dos tradicionais sushis.<br> O restaurante também conta com um bar preparado para servir os melhores drinks e uma carta de vinhos que promete atender todos os gostos.<br><br><br> Av. Dom Luís, 1113, Shopping Buaganvília - Meireles, Fortaleza",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3435459297984!2d-38.493792325026185!3d-3.735098396238809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c7487dcb70bd3d%3A0xa6025a1ec2451c57!2sRyori%20-%20Shopping%20Buganv%C3%ADlia!5e0!3m2!1spt-BR!2sbr!4v1702997844865!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/ceara/imgCeara/img_ceara_redimensionadas/carboneSteakhouse.jpg",
+    nomeRestaurante: "Carbone Steakhouse",
+    descricaoRestaurante:" Especializado no preparo de carnes, o Carbone se inspira no fogo para entregar uma verdadeira experiência para aqueles que valorizam bons momentos.<br> Não limitando-se ao preparo de carnes, o Carbone busca no fogo a fagulha criativa da sua cozinha.<br> Afinal de contas o Fogo é o elemento que permitiu aos homens iluminar os caminhos desconhecidos, forjar novas possibilidades e compartilharem histórias.<br> Explorar, produzir e dividir, é isso que alimenta chama do Carbone, inspirando a transformar o natural em algo extraordinário.<br>Proporcionar bons momentos em torno do fogo, esse é o verdadeiro propósito do Carbone.<br><br><br><br><br> Av. Des. Moreira, 1300 - Aldeota,<br> Fortaleza",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.3334564574725!2d-38.50157512502601!3d-3.737321896236586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c749c86261f893%3A0x84c772a1964d1fde!2sCarbone%20Steakhouse!5e0!3m2!1spt-BR!2sbr!4v1702999985665!5m2!1spt-BR!2sbr",
+  },
+];
+
+// CAPTURANDO O ELEMENTO
+// Esta parte do código espera até que o conteúdo da página (DOM) esteja completamente carregado antes de executar o código dentro da função. Quando o DOMContentLoaded é acionado, a função é chamada.
+// A linha seguinte obtém a referência ao elemento com o ID "dicas-restaurantes", que é o contêiner onde as dicas de restaurantes serão exibidas.
+document.addEventListener("DOMContentLoaded", function () {
+let dicasRestaurantes = document.getElementById("dicas-restaurantes");
+
+// EBARALHANDO O ARRAY
+//  Essa parte do código embaralha o arrayDicasRestaurantes.
+//  O método sort é usado para embaralhar o array, e a função de comparação Math serve para retornar um número positivo ou negativo aleatório, resultando no embaralhamento aleatório do array.
+const arrayDicasEmbaralhadas = arrayDicasRestaurantes.sort(
+  () => Math.random() - 0.5
+);
+
+// EXIBINDO DUAS DICAS POR VEZ
+// Aqui, definimos algumas variáveis para controlar a quantidade de dicas exibidas por vez (dicasPorSlide),
+// o número total de dicas disponíveis (totalDicas), e o índice atual da dica a ser exibida (indiceDicaAtual).
+let dicasPorSlide = 2;
+let totalDicas = arrayDicasEmbaralhadas.length;
+let dicaAtual = 0;
+
+// Função para criar e exibir as entradas do restaurante
+// A função exibirEntradasDoRestaurante é responsável por limpar o conteúdo atual do elemento dicasRestaurantes,
+// garantindo que as novas dicas sejam exibidas sem duplicatas.
+function exibirEntradasDoRestaurante() {
+  // Limpar o conteúdo atual
+  dicasRestaurantes.innerHTML = "";
+
+  // Loop para exibir dicasPorSlide número de dicas
+  for (let i = 0; i < dicasPorSlide; i++) {
+    // Calcular o índice atual da dica
+    let indice = (dicaAtual + i) % totalDicas;
+
+    // Criando uma nova div para cada entrada de restaurante
+    let novaEntrada = document.createElement("div");
+    novaEntrada.classList.add("transicao");
+
+    // Definir innerHTML para a nova div
+    novaEntrada.innerHTML = `
+<div>                
+  <div>
+      <img class="img-indicacoes" src="${arrayDicasEmbaralhadas[indice].imagem}" alt="">  
+  </div>
+
+  <div class="conteudo-restaurantes">
+      <h3 class="nome-estabelecimento">${arrayDicasEmbaralhadas[indice].nomeRestaurante}</h3>
+      <br>
+      <p class="descricao-estabelecimento">${arrayDicasEmbaralhadas[indice].descricaoRestaurante}</p>   
+  </div> 
+  <iframe class="fachada-maps" src="${arrayDicasEmbaralhadas[indice].mapa}"></iframe>
+</div>
+`;
+
+    // Anexando a nova entrada ao elemento capturado
+    dicasRestaurantes.appendChild(novaEntrada);
+
+    novaEntrada.offsetWidth;
+    novaEntrada.classList.add("active");
+  }
+
+  // Atualizar o índiceDicaAtual para o próximo conjunto de dicas
+  dicaAtual = (dicaAtual + dicasPorSlide) % totalDicas;
+
+  setTimeout(exibirEntradasDoRestaurante, 5000);
+}
+
+// Exibição inicial de entradas de restaurante
+exibirEntradasDoRestaurante();
+});
