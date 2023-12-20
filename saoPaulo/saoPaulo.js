d17022f0bfa4b584cb6c89b444f6b3b1c83ee81b
Caic Rangel <caicrangel@gmail.com>
2023-12-20 09:07:33 -0300

    Ajustando a estrutura de Sao Paulo

---
+++ saoPaulo/saoPaulo.js
@@ -0,0 +1,108 @@
+const arrayDicasRestaurantes = [
+  {
+    imagem: "/saoPaulo/imgSaoPaulo/img_10_sp.jpg",
+    nomeRestaurante: "Mercadão de São Paulo",
+    descricaoRestaurante:
+      "O Mercadão de São Paulo, oficialmente conhecido como o Mercado Municipal Paulistano, é um ícone cultural e gastronômico na cidade. Localizado no coração de São Paulo, esse mercado histórico abriga uma vasta variedade de barracas e lojas que oferecem uma infinidade de produtos, desde frutas frescas e legumes exóticos até queijos de qualidade e carnes selecionadas. Uma de suas atrações mais famosas é o sanduíche de mortadela, que é uma verdadeira iguaria e uma obrigação para os visitantes. Além disso, a arquitetura do Mercadão é impressionante, com seus vitrais coloridos que proporcionam uma atmosfera única. É um local que celebra a rica diversidade cultural de São Paulo e oferece uma experiência sensorial única para os amantes da gastronomia.<br><br><br> Rua da Cantareira, 306 - Centro Histórico de São Paulo, São Paulo",
+    mapa: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14630.970910511764!2d-46.630082!3d-23.541754!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5f8c61020205%3A0x9ae474c606fef4!2sMercado%20Municipal%20de%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1702853216820!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/saoPaulo/imgSaoPaulo/img_12_sp.jpg",
+    nomeRestaurante: "Bar Dona Onça",
+    descricaoRestaurante:
+      "O Bar da Dona Onça em São Paulo é um ícone da cena gastronômica da cidade. Localizado no centro histórico, esse estabelecimento aconchegante e charmoso combina com maestria a atmosfera de um autêntico boteco com a sofisticação da alta culinária. Sob a liderança da chef Janaína Rueda, o cardápio oferece uma mistura de pratos tradicionais brasileiros e contemporâneos, destacando-se por sua famosa coxinha de galinha, que é considerada uma das melhores da cidade. Além disso, o ambiente acolhedor, com uma decoração que homenageia a era de ouro do samba, faz com que o Bar da Dona Onça seja um local preferido tanto para locais quanto para turistas que desejam desfrutar de uma experiência gastronômica autêntica e única em São Paulo.<br><br><br> Av. Ipiranga, 200 - 27 e 29 - Centro Histórico de São Paulo, São Paulo",
+    mapa: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14630.441956588125!2d-46.644532!3d-23.546508!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce584e8dad2313%3A0x858d1be47f822f26!2sBar%20da%20Dona%20On%C3%A7a!5e0!3m2!1spt-BR!2sbr!4v1702853347669!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/saoPaulo/imgSaoPaulo/imakay.jpg",
+    nomeRestaurante: "Imakay Restaurante",
+    descricaoRestaurante:"A gastronomia japonesa tradicional acrescida de elementos de cozinha autoral é o foco do restaurante Imakay, na capital paulista. Seu cardápio conta com sushis feitos de pescados maturados, carpaccio de jamón de atum feito da casa, usuzukuri (técnica milenar japonesa que consiste em finos cortes como lâminas de peixes finíssimas – peixe branco com molho ponzu) e muito mais.<br><br><br><br><br><br><br><br><br><br> Rua Urussuí, 330 – Itaim Bibi,<br> São Paulo.",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.498335795475!2d-46.67895552466921!3d-23.58645437878264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce579f0fa09675%3A0x601a15d63d4a8326!2sImakay%3A%20Temaki%2C%20Sashimis%2C%20Parrilla%2C%20Sak%C3%AA%2C%20Drinks%2C%20Sobremesas%2C%20Delivery%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1702928619001!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem:"/saoPaulo/imgSaoPaulo/tasteItRestaurante.jpg",
+    nomeRestaurante: "TasteIt Restaurante",
+    descricaoRestaurante: "Considerado um dos melhores restaurantes de São Paulo segundo o ranking do TripAdvisor, o TasteIT fica localizado dentro do Pullman SP Ibirapuera e propõe uma experiência de gastronomia fusion entre clássicos franceses e ingredientes brasileiros que vão além da comida.<br> Seja no contato com louças do artesanato local, com a boa música das atrações, ou drinks e pratos com ingredientes orgânicos, você pode ter certeza de que terá uma noite animada e cheia de sabores.<br><br><br><br><br><br><br><br> Rua Joinville, 515 - Vila Mariana,<br> São Paulo",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.6121122938507!2d-46.65278322466928!3d-23.58237067878528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce598d59356ee3%3A0xf7ac1aee1ab3e062!2sTastelT%20Restaurante!5e0!3m2!1spt-BR!2sbr!4v1702989446982!5m2!1spt-BR!2sbr",
+  },
+
+  {
+    imagem: "/saoPaulo/imgSaoPaulo/ranchoDosPeruanos.jpg",
+    nomeRestaurante: "Rancho dos Peruanos",
+    descricaoRestaurante:
+      "Para quem deseja ter uma autêntica experiência em um restaurante peruano em São Paulo pode visitar o Rancho dos Peruanos. Situada na Mooca, a casa inaugurada em 2022 oferece mais de 40 opções da gastronomia peruana em seu cardápio.<br> O primeiro destaque fica por conta do Ceviche, o prato mais conhecido dessa culinária. Feito com peixe cru marinado em leite de tigre, uma mistura de temperos com suco de limão, ainda leva pimentas, cebola roxa e coentro. Ou seja, uma mistura de sabores frescos e picantes.<br><br><br><br><br><br><br> Rua da Mooca, 3475 - Mooca,<br> São Paulo",
+    mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.2975929324143!2d-46.59433052467006!3d-23.55775307880231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5ffd83830fdf%3A0xe6b14b83b91a7db5!2sRancho%20dos%20Peruanos!5e0!3m2!1spt-BR!2sbr!4v1702990545884!5m2!1spt-BR!2sbr",
+  },
+];
+
+// CAPTURANDO O ELEMENTO
+// Esta parte do código espera até que o conteúdo da página (DOM) esteja completamente carregado antes de executar o código dentro da função. Quando o DOMContentLoaded é acionado, a função é chamada.
+// A linha seguinte obtém a referência ao elemento com o ID "dicas-restaurantes", que é o contêiner onde as dicas de restaurantes serão exibidas.
+document.addEventListener("DOMContentLoaded", function () {
+  let dicasRestaurantes = document.getElementById("dicas-restaurantes");
+
+  // EBARALHANDO O ARRAY
+  //  Essa parte do código embaralha o arrayDicasRestaurantes.
+  //  O método sort é usado para embaralhar o array, e a função de comparação Math serve para retornar um número positivo ou negativo aleatório, resultando no embaralhamento aleatório do array.
+  const arrayDicasEmbaralhadas = arrayDicasRestaurantes.sort(
+    () => Math.random() - 0.5
+  );
+
+  // EXIBINDO DUAS DICAS POR VEZ
+  // Aqui, definimos algumas variáveis para controlar a quantidade de dicas exibidas por vez (dicasPorSlide),
+  // o número total de dicas disponíveis (totalDicas), e o índice atual da dica a ser exibida (indiceDicaAtual).
+  let dicasPorSlide = 2;
+  let totalDicas = arrayDicasEmbaralhadas.length;
+  let dicaAtual = 0;
+
+  // Função para criar e exibir as entradas do restaurante
+  // A função exibirEntradasDoRestaurante é responsável por limpar o conteúdo atual do elemento dicasRestaurantes,
+  // garantindo que as novas dicas sejam exibidas sem duplicatas.
+  function exibirEntradasDoRestaurante() {
+    // Limpar o conteúdo atual
+    dicasRestaurantes.innerHTML = "";
+
+    // Loop para exibir dicasPorSlide número de dicas
+    for (let i = 0; i < dicasPorSlide; i++) {
+      // Calcular o índice atual da dica
+      let indice = (dicaAtual + i) % totalDicas;
+
+      // Criando uma nova div para cada entrada de restaurante
+      let novaEntrada = document.createElement("div");
+      novaEntrada.classList.add("transicao");
+
+      // Definir innerHTML para a nova div
+      novaEntrada.innerHTML = `
+<div>                
+    <div>
+        <img class="img-indicacoes" src="${arrayDicasEmbaralhadas[indice].imagem}" alt="">  
+    </div>
+
+    <div class="conteudo-restaurantes">
+        <h3 class="nome-estabelecimento">${arrayDicasEmbaralhadas[indice].nomeRestaurante}</h3>
+        <br>
+        <p class="descricao-estabelecimento">${arrayDicasEmbaralhadas[indice].descricaoRestaurante}</p>   
+    </div> 
+    <iframe class="fachada-maps" src="${arrayDicasEmbaralhadas[indice].mapa}"></iframe>
+</div>
+`;
+
+      // Anexando a nova entrada ao elemento capturado
+      dicasRestaurantes.appendChild(novaEntrada);
+
+      novaEntrada.offsetWidth;
+      novaEntrada.classList.add("active");
+    }
+
+    // Atualizar o índiceDicaAtual para o próximo conjunto de dicas
+    dicaAtual = (dicaAtual + dicasPorSlide) % totalDicas;
+
+    setTimeout(exibirEntradasDoRestaurante, 5000);
+  }
+
+  // Exibição inicial de entradas de restaurante
+  exibirEntradasDoRestaurante();
+});
