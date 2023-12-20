623656cdb0fa1c35be5209033c28eb3c37788cf9
Caic Rangel <caicrangel@gmail.com>
2023-12-20 08:43:47 -0300

    Alterando toda a estrutura da pasta Minas Gerais

---
+++ minasGerais/minasGerais.js
@@ -0,0 +1,106 @@
+const arrayDicasRestaurantes = [
+    {
+      imagem: "/minasGerais/imgMinasGerais/pao-de-queijaria-savassi.png",
+      nomeRestaurante: "A Pão de Queijaria",
+      descricaoRestaurante: "A pergunta que mais ouvimos desde a nossa inauguração é: “mas de onde surgiu a ideia da A Pão de Queijaria? Precisamos confessar que nem sempre a resposta mais honesta – embora seja a mais óbvia – é dita ao nosso querido interlocutor.<br> Mas a verdade é que A Pão de Queijaria surgiu há muitos e muitos anos, dentro das cozinhas de milhares de famílias mineiras que, de geração em geração, um dia ousaram misturar queijo com polvilho.<br> Por força do acaso ou não, fundaram um dos pilares da gastronomia mais autêntica do Brasil.<br><br><br><br> R. Antônio de Albuquerque, 856 - Funcionários, Belo Horizonte",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3750.6933365015198!2d-43.94096582382043!3d-19.937323638538523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699da230c347f%3A0xff55f7e4c8b15de8!2sA%20P%C3%A3o%20de%20Queijaria%20-%20Savassi!5e0!3m2!1spt-BR!2sbr!4v1702424046057!5m2!1spt-BR!2sbr",
+    },
+  
+    {
+      imagem:"/minasGerais/imgMinasGerais/mercado_central.jpg",
+      nomeRestaurante: "Dona Diva Café e Quitandas",
+      descricaoRestaurante: "A Dona Diva Café e Quitandas é uma quitanderia localizada no Mercado Central de Belo Horizonte e prima por ingredientes de qualidade e de origem certificada.<br> Os produtos são artesanais e não levam aditivos químicos, essências e misturas prontas.<br> O pão de queijo feito com o legítimo queijo da Serra da Canastra é o destaque do estabelecimento e já foi premiado por diversas publicações especializadas em gastronomia.<br><br><br><br><br> Av. Augusto de Lima, 744 - Loja 116 - Centro, Belo Horizonte",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.0380595246615!2d-43.94564792382074!3d-19.922800938086166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699e219fa4b2d%3A0x4ab38a2629d172b7!2sDONA%20DIVA%20CAF%C3%89%20E%20QUITANDAS!5e0!3m2!1spt-BR!2sbr!4v1702424145216!5m2!1spt-BR!2sbr",
+    },
+  
+    {
+      imagem: "/minasGerais/imgMinasGerais/restauranteCasaCheia.jpg",
+      nomeRestaurante: "Restaurante Casa Cheia",
+      descricaoRestaurante: "O restaurante, que funciona desde 1978, é tradição entre os belo horizontinos e já virou ponto turístico para quem vem de fora visitar a cidade.<br> Entre os pratos mais famosos está o 'Mexidoido Chapado'- carro chefe da casa,  Petiscos e pratos típicos da cozinha mineira.<br><br><br><br><br><br><br><br><br> Av. Augusto de Lima, 744 - Loja 167 - Centro, Belo Horizonte",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3751.0497219662!2d-43.945105923820854!3d-19.92230943807103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699e2110736d1%3A0x99cd4f0e23e186d1!2sCasa%20Cheia%20-%20Mercado%20Central!5e0!3m2!1spt-BR!2sbr!4v1702424789336!5m2!1spt-BR!2sbr",
+    },
+  
+    {
+      imagem: "/minasGerais/imgMinasGerais/restauranteXapuri.jpg",
+      nomeRestaurante: "Restaurante Xapuri",
+      descricaoRestaurante: "Tradição na cidade, o premiado Xapuri proporciona experiências gastronômicas com mineiridade.<br> Afastado do centro, o restaurante é um verdadeiro refúgio para a mente. Pratos autênticos e saborosos servidos à la carte, ambiente rústico e aconchegante, espaço kids, loja de artesanato e hípica.<br> Buffet de doces caseiros, com mais de 30 variedades. Os almoços de sábados, domingos e feriados e os jantares de quarta-feira a sábado, são regados de músicas regionais ao som de viola e sanfona do Trio Xapuri.<br><br><br><br> Rua Mandacarú, 260 - Loja 1 -   Trevo,    Belo<br> Horizonte",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3752.956826021577!2d-44.0059786238227!3d-19.841779635569345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa699e4fa6389b9%3A0xbaa584c94ff8f8dd!2sRestaurante%20Xapuri!5e0!3m2!1spt-BR!2sbr!4v1702424982480!5m2!1spt-BR!2sbr",
+    },
+  
+    {
+      imagem: "/minasGerais/imgMinasGerais/restauranteCaipiraChique.jpg",
+      nomeRestaurante: "Restaurante Caipira Chique",
+      descricaoRestaurante: "Restaurante especializado em Gastronomia Mineira.<br> Com serviço exclusivamente 'a la carte' tem em seu cardápio clássicos como frango com quiabo, torresmo de barriga, carne de sereno entre outras delícias da cozinha comandada pelo Chef Leo Cabral e sua mãe Lena.<br><br><br><br><br><br><br><br><br> R. Francisco Bretas Bering, 324 - Copacabana, Belo Horizonte ",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3753.066337981303!2d-43.99477292382287!3d-19.837145835425787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa691c43a5a22e3%3A0x41663fbffafa5220!2sCaipira%20Xique%20Restaurante!5e0!3m2!1spt-BR!2sbr!4v1702425156799!5m2!1spt-BR!2sbr",
+    },
+  ];
+
+// CAPTURANDO O ELEMENTO
+// Esta parte do código espera até que o conteúdo da página (DOM) esteja completamente carregado antes de executar o código dentro da função. Quando o DOMContentLoaded é acionado, a função é chamada.
+// A linha seguinte obtém a referência ao elemento com o ID "dicas-restaurantes", que é o contêiner onde as dicas de restaurantes serão exibidas.
+document.addEventListener("DOMContentLoaded", function () {
+    let dicasRestaurantes = document.getElementById("dicas-restaurantes");
+  
+    // EBARALHANDO O ARRAY
+    //  Essa parte do código embaralha o arrayDicasRestaurantes.
+    //  O método sort é usado para embaralhar o array, e a função de comparação Math serve para retornar um número positivo ou negativo aleatório, resultando no embaralhamento aleatório do array.
+    const arrayDicasEmbaralhadas = arrayDicasRestaurantes.sort(
+      () => Math.random() - 0.5
+    );
+  
+    // EXIBINDO DUAS DICAS POR VEZ
+    // Aqui, definimos algumas variáveis para controlar a quantidade de dicas exibidas por vez (dicasPorSlide),
+    // o número total de dicas disponíveis (totalDicas), e o índice atual da dica a ser exibida (indiceDicaAtual).
+    let dicasPorSlide = 2;
+    let totalDicas = arrayDicasEmbaralhadas.length;
+    let dicaAtual = 0;
+  
+    // Função para criar e exibir as entradas do restaurante
+    // A função exibirEntradasDoRestaurante é responsável por limpar o conteúdo atual do elemento dicasRestaurantes,
+    // garantindo que as novas dicas sejam exibidas sem duplicatas.
+    function exibirEntradasDoRestaurante() {
+      // Limpar o conteúdo atual
+      dicasRestaurantes.innerHTML = "";
+  
+      // Loop para exibir dicasPorSlide número de dicas
+      for (let i = 0; i < dicasPorSlide; i++) {
+        // Calcular o índice atual da dica
+        let indice = (dicaAtual + i) % totalDicas;
+  
+        // Criando uma nova div para cada entrada de restaurante
+        let novaEntrada = document.createElement("div");
+        novaEntrada.classList.add("transicao");
+  
+        // Definir innerHTML para a nova div
+        novaEntrada.innerHTML = `
+  <div>                
+      <div>
+          <img class="img-indicacoes" src="${arrayDicasEmbaralhadas[indice].imagem}" alt="">  
+      </div>
+  
+      <div class="conteudo-restaurantes">
+          <h3 class="nome-estabelecimento">${arrayDicasEmbaralhadas[indice].nomeRestaurante}</h3>
+          <br>
+          <p class="descricao-estabelecimento">${arrayDicasEmbaralhadas[indice].descricaoRestaurante}</p>   
+      </div> 
+      <iframe class="fachada-maps" src="${arrayDicasEmbaralhadas[indice].mapa}"></iframe>
+  </div>
+  `;
+  
+        // Anexando a nova entrada ao elemento capturado
+        dicasRestaurantes.appendChild(novaEntrada);
+  
+        novaEntrada.offsetWidth;
+        novaEntrada.classList.add("active");
+      }
+  
+      // Atualizar o índiceDicaAtual para o próximo conjunto de dicas
+      dicaAtual = (dicaAtual + dicasPorSlide) % totalDicas;
+  
+      setTimeout(exibirEntradasDoRestaurante, 5000);
+    }
+  
+    // Exibição inicial de entradas de restaurante
+    exibirEntradasDoRestaurante();
+  });  
+  
