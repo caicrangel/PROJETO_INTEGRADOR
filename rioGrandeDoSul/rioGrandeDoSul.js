1031be0b0aadf82627ef4b4de7fd31e7df07ed52
Caic Rangel <caicrangel@gmail.com>
2023-12-20 08:31:52 -0300

    Adicionando a pasta Rio Grande do Sul

---
+++ rioGrandeDoSul/rioGrandeDoSul.js
@@ -0,0 +1,106 @@
+const arrayDicasRestaurantes = [
+    {
+      imagem:"/rioGrandeDoSul/imgRioGrandeDoSul/freio-de-ouro-churrascaria01.jpg",
+      nomeRestaurante: "Churrascaria Freio de Ouro",
+      descricaoRestaurante:"Foi inaugurada em Porto Alegre no ano de 2009 e vem atingindo às exigências da demanda em serviços gastronômicos e assim tornou-se uma das principais churrascarias na capital gaúcha. <br> E consolidando sua missão, a Churrascaria Freio de Ouro oferece um variado rodízio de carnes com cortes nobres, buffet de grelhados, pratos quentes, saladas, sobremesas e uma carta de vinhos.<br><br> O rodízio da Freio de Ouro conta com picanha, paleta de ovelha, vazio, cupim e lombo. Para os apaixonados por costela, um dos destaques é o Costelão 12 horas, assado em fogo brando por meio dia à perfeição. Para acompanhar, há um buffet de grelhados e de pratos quentes.<br><br><br><br> Av. José de Alencar, 460 - Menino de Deus, Porto Alegre",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2903.759239539663!2d-51.22922231331373!3d-30.060628298664557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951978f34e04c31f%3A0xce712ae2e9e764e0!2sChurrascaria%20Freio%20de%20Ouro!5e0!3m2!1spt-BR!2sbr!4v1700333789370!5m2!1spt-BR!2sbr",
+    },
+
+    {
+      imagem: "/rioGrandeDoSul/imgRioGrandeDoSul/churrascaria barranco01.jpg",
+      nomeRestaurante: "Churrascaria Barranco",
+      descricaoRestaurante:"Praticamente uma instituição da cidade, a Churrascaria Barranco faz parte da história de quem vive na capital dos gaúchos e deixa saudade em quem vem de fora e a visita. <br> Nessa casa acolhedora, aberta todos os dias do ano no almoço e no jantar, há espaço para as mais diversas ocasiões: do encontro com amigos até aquele almoço com a família para celebrar aniversários e outras alegrias. <br> <br> Fundado em 1969 no bairro Petrópolis, o restaurante tem mesas ao ar livre que convidam os clientes a comer sem pressa – apreciando o chope, que está sempre bem gelado – e a ficar horas conversando sob as árvores, que à noite, quando as luzinhas se acendem, deixam o clima mais charmoso.<br><br><br>  Av. Protásio Alves, 1.578 - Petrópolis, Porto Alegre<br><br>",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.8688214190574!2d-51.194823123524586!3d-30.040621031100866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519783b017471cb%3A0x90d23152ced4127f!2sChurrascaria%20Barranco!5e0!3m2!1spt-BR!2sbr!4v1700333960900!5m2!1spt-BR!2sbr",
+    },
+
+    {
+      imagem: "/rioGrandeDoSul/imgRioGrandeDoSul/Parrilla del Sur01.jpg",
+      nomeRestaurante: "Parrilla del Sur",
+      descricaoRestaurante:"Para reunir familiares e amigos ou para fechar negócios a qualquer hora do dia e sem hora para ir embora, a Parrilla del Sur é um ótimo lugar para quem busca bons assados e o toque da gastronomia uruguaia. Localizado em um dos pontos mais nobres da cidade, o restaurante se destaca pelas carnes assadas na grelha, bem pertinho da brasa, garantindo o sabor e a suculência legítimos de uma parrilla uruguaia.<br><br>O salão para mais de 400 pessoas é contemporâneo e passa uma impressão rústica, comum aos restaurantes do Uruguai, o que dá certo charme ao espaço. Nele, há área de lazer com supervisor para a criançada.<br><br><br><br><br><br> Avenida Nilópolis, 111 - Petrópolis, Porto Alegre<br><br>",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3453.933740268805!2d-51.19325542352469!3d-30.038758731014543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9519783ab9aab35b%3A0x5c5d2728431d4e2b!2sParrilla%20Del%20Sur!5e0!3m2!1spt-BR!2sbr!4v1701135226404!5m2!1spt-BR!2sbr",
+    },
+
+    {
+      imagem:
+        "/rioGrandeDoSul/imgRioGrandeDoSul/Churrascaria-Chama-de-Fogo01.jpg",
+      nomeRestaurante: "Churrascaria Chama de Fogo",
+      descricaoRestaurante:"Tchê, se tu procuras o verdadeiro churrasco gaúcho, a Churrascaria Chama de Fogo é o lugar certo. Muito além do melhor churrasco da Serra Gaúcha, a Chama de fogo proporciona uma autentica experiência gastronômica.Com uma localização privilegiada, a churrascaria fica na principal avenida da cidade, a Borges de Medeiros, é de fácil acesso e possui estacionamento privativo.<br><br>Famosa pela qualidade das carnes, os cortes oferecidos estão desde a tradicional Picanha até cortes especiais como o  Shoulder Steak, Prime Rib,  Bombom da Alcatra, Ancho (Ojo de bife), Costela Premium, Bife de Chorizo e Assado de tiras, Costela suína, Costela Gaúcha assada em fogo brando e cortes especiais de cordeiro como o Carré francês e a Paleta.<br><br><br>Av. Borges de Medeiros, 1350 - Planalto, Gramado <br><br>",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3476.3423242237645!2d-50.87589692354657!3d-29.389540301219004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95193238975bf223%3A0x79b98c6a337d92db!2sChurrascaria%20Chama%20de%20Fogo%20Gramado!5e0!3m2!1spt-BR!2sbr!4v1701136940034!5m2!1spt-BR!2sbr",
+    },
+
+    {
+      imagem: "/rioGrandeDoSul/imgRioGrandeDoSul/churrascaria imperador01.jpg",
+      nomeRestaurante: "Churrascaria Imperador",
+      descricaoRestaurante:"Em 1970, os casais Pedro e Terezinha Regalin e Pedro e Luiza Rizzi, acreditaram que o futuro da família estaria em um Restaurante.<br><br> Mal sabiam eles que a sabedoria gaúcha de assar carnes, aliada a cultura italiana, herdada dos imigrantes, dariam a Caxias do Sul um dos restaurantes mais respeitados do Rio Grande do Sul.<br><br> É esta união de sabores e temperos que fazem da Churrascaria Imperador um lugar perfeito para confraternizar e comemorar, provando o mais delicioso Churrasco Imperial da Serra Gaúcha.<br><br><br><br><br><br> Av. Júlio de Castilhos, 3107 - São Pelegrino, Caxias do Sul",
+      mapa: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3483.888863226981!2d-51.196277223553814!3d-29.167947091189045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951ea32721e485b5%3A0xd78e21494b58e5e7!2sChurrascaria%20Imperador!5e0!3m2!1spt-BR!2sbr!4v1701216038678!5m2!1spt-BR!2sbr",
+    },
+  ];
+
+// CAPTURANDO O ELEMENTO
+// Esta parte do código espera até que o conteúdo da página (DOM) esteja completamente carregado antes de executar o código dentro da função. Quando o DOMContentLoaded é acionado, a função é chamada.
+// A linha seguinte obtém a referência ao elemento com o ID "dicas-restaurantes", que é o contêiner onde as dicas de restaurantes serão exibidas.
+document.addEventListener("DOMContentLoaded", function () {
+  let dicasRestaurantes = document.getElementById("dicas-restaurantes");
+
+  // EBARALHANDO O ARRAY
+  //  Essa parte do código embaralha o arrayDicasRestaurantes.
+  //  O método sort é usado para embaralhar o array, e a função de comparação Math serve para retornar um número positivo ou negativo aleatório, resultando no embaralhamento aleatório do array.
+  const arrayDicasEmbaralhadas = arrayDicasRestaurantes.sort(
+    () => Math.random() - 0.5
+  );
+
+  // EXIBINDO DUAS DICAS POR VEZ
+  // Aqui, definimos algumas variáveis para controlar a quantidade de dicas exibidas por vez (dicasPorSlide),
+  // o número total de dicas disponíveis (totalDicas), e o índice atual da dica a ser exibida (indiceDicaAtual).
+  let dicasPorSlide = 2;
+  let totalDicas = arrayDicasEmbaralhadas.length;
+  let dicaAtual = 0;
+
+  // Função para criar e exibir as entradas do restaurante
+  // A função exibirEntradasDoRestaurante é responsável por limpar o conteúdo atual do elemento dicasRestaurantes,
+  // garantindo que as novas dicas sejam exibidas sem duplicatas.
+  function exibirEntradasDoRestaurante() {
+    // Limpar o conteúdo atual
+    dicasRestaurantes.innerHTML = "";
+
+    // Loop para exibir dicasPorSlide número de dicas
+    for (let i = 0; i < dicasPorSlide; i++) {
+      // Calcular o índice atual da dica
+      let indice = (dicaAtual + i) % totalDicas;
+
+      // Criando uma nova div para cada entrada de restaurante
+      let novaEntrada = document.createElement("div");
+      novaEntrada.classList.add("transicao");
+
+      // Definir innerHTML para a nova div
+      novaEntrada.innerHTML = `
+<div>                
+    <div>
+        <img class="img-indicacoes" src="${arrayDicasEmbaralhadas[indice].imagem}" alt="">  
+    </div>
+
+    <div class="conteudo-restaurantes">
+        <h3 class="nome-estabelecimento">${arrayDicasEmbaralhadas[indice].nomeRestaurante}</h3>
+        <br>
+        <p class="descricao-estabelecimento">${arrayDicasEmbaralhadas[indice].descricaoRestaurante}</p>   
+    </div> 
+    <iframe class="fachada-maps" src="${arrayDicasEmbaralhadas[indice].mapa}"></iframe>
+</div>
+`;
+
+      // Anexando a nova entrada ao elemento capturado
+      dicasRestaurantes.appendChild(novaEntrada);
+
+      novaEntrada.offsetWidth;
+      novaEntrada.classList.add("active");
+    }
+
+    // Atualizar o índiceDicaAtual para o próximo conjunto de dicas
+    dicaAtual = (dicaAtual + dicasPorSlide) % totalDicas;
+
+    setTimeout(exibirEntradasDoRestaurante, 5000);
+  }
+
+  // Exibição inicial de entradas de restaurante
+  exibirEntradasDoRestaurante();
+});
